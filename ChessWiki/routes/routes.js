0b0c8428bb9b84959cad48f58e42da4b90c4e943
daniel <daack001>
2021-02-24 19:44:35 +0100

    position.ejs erstellt und gerouted

--- ChessWiki/routes/routes.js
+++ ChessWiki/routes/routes.js
@@ -2,16 +2,25 @@ const express = require("express");
 const router = express.Router();
 const positions = require("../models/positions");
 
-router.get("/", function(req,res) {
+router.get("/", function (req, res) {
     res.render("choose");
-})
+});
 
 router.get("/listpositions", function (req, res) {
-    res.render("listpositions", {positions: positions.slice(0,4)});
+    res.render("listpositions", {positions: positions.slice(0, 4)});
+});
+router.get("/listpositions/position1", function (req, res) {
+    res.render("position", {positions: positions[0]});
+});
+router.get("/listpositions/position2", function (req, res) {
+    res.render("position", {positions: positions[1]});
+});
+router.get("/listpositions/position3", function (req, res) {
+    res.render("position", {positions: positions[2]});
 });
 
 router.get("/listofopenings", function (req, res) {
     res.render("listofopenings");
-})
+});
 
 module.exports = router;
