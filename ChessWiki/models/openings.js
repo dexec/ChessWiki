23b49b7af1516ce8d49a20ec6b0d2a5d7ccd3f55
algro004 <alexander.gromov004@stud.fh-dortmund.de>
2021-03-09 17:52:27 +0100

    paar experimente und openings erweitert

--- ChessWiki/models/openings.js
+++ ChessWiki/models/openings.js
@@ -1,4 +1,4 @@
-function Opening(ecoCat, ecoSubcat, parent, name, moves, desc) {
+function Opening(ecoCat, ecoSubcat, parent, name, variations, moves, desc) {
 
     this.ecoCat = ecoCat;
     this.ecoSubcat = ecoSubcat;
@@ -14,8 +14,24 @@ function Opening(ecoCat, ecoSubcat, parent, name, moves, desc) {
     this.getName = function () {
         return name;
     }
+
+    this.getDesc = function () {
+        return desc;
+    }
+
+    this.getParent = function () {
+        return parent;
+    }
+
+    this.getMoves = function () {
+        return moves;
+    }
+
+    this.getVariations = function () {
+        return variations;
+    }
 }
 
-let schottische_partie = new Opening("C", 45, "Königsspringerspiel", "Schottische Partie", ["e4","e5","Nf3","Nc6","d4"], "Die Schottische Partie ist eine offene Eröffnung und entwickelt sich aus dem Königsspringerspiel, manchmal auch aus dem Mittelgambit. Die Eröffnung hat neben ihrer Hauptvariante nur wenige Nebenvarianten und Gambits, weswegen man die Theorie schnell erlernen kann und die Eröffnung nur in ECO-Nummer C45 vorzufinden ist.");
+let schottische_partie = new Opening("C", 45, "Königsspringerspiel", "Schottische Partie",  ["Hauptvariante", "Schmidt-Variante", "Schottisches Gambit"], ["e4","e5","Nf3","Nc6","d4"], "Die Schottische Partie ist eine offene Eröffnung und entwickelt sich aus dem Königsspringerspiel, manchmal auch aus dem Mittelgambit. Die Eröffnung hat neben ihrer Hauptvariante nur wenige Nebenvarianten und Gambits, weswegen man die Theorie schnell erlernen kann und die Eröffnung nur in ECO-Nummer C45 vorzufinden ist.");
 
 module.exports = [schottische_partie];
--- ChessWiki/routes/routes.js
+++ ChessWiki/routes/routes.js
@@ -9,17 +9,26 @@ const positionOfTheDay = positionsForPosition[1];
 const positionsCounter = 4;
 router.use(bodyParser.urlencoded({extended: false}));
 
+function positionRender() {
+    for (let i = 0; i < positionsForPosition.length; i++) {
+        console.log(i);
+        router.get("/positions/position" + i, function (req, res) {
+            res.render("position", {position: positionsForPosition[i], positionOfTheDay: positionOfTheDay});
+        });
+    }
+}
+
 router.post("/formposition", function (req, res) {
     positionString = req.body.row8 + "/" + req.body.row7 + "/" + req.body.row6 + "/" + req.body.row5 + "/" + req.body.row4 + "/" + req.body.row3 + "/" + req.body.row2 + "/" + req.body.row1;
     activeColor = req.body.activeColor;
     castlingAvailability = req.body.castlingAvailability;
     numberOfMoves = req.body.numberOfMoves;
-    console.log(positionString);
     positionsForPosition.push(new dataPositions.Position(positionString, activeColor, castlingAvailability, numberOfMoves));
-    //res.render("position", {
-    //  position: positionsForPosition[positionsForPosition.length - 1],
-    //   positionOfTheDay: positionOfTheDay
-    //});
+    /*res.render("position", {
+        position: positionsForPosition[positionsForPosition.length - 1],
+        positionOfTheDay: positionOfTheDay
+    });*/
+    positionRender();
     res.redirect("/positions");
 })
 
@@ -31,11 +40,7 @@ router.get("/positions", function (req, res) {
     res.render("listpositions", {positions: positionsForPosition, positionOfTheDay: positionOfTheDay});
 });
 
-for (let i = 0; i < positionsForPosition.length; i++) {
-    router.get("/positions/position" + i, function (req, res) {
-        res.render("position", {position: positionsForPosition[i], positionOfTheDay: positionOfTheDay});
-    })
-}
+positionRender();
 
 router.get("/openings", function (req, res) {
     res.render("listopenings", {positionOfTheDay: positionOfTheDay});
