1ff207b641398248a6598d0af3601d2822394509
daniel <daack001>
2021-03-10 19:56:58 +0100

    Dokumentation weiter bearbeitet, required in formposition hinzugefügt, das rendern von positions ohne function gelöst

--- ChessWiki/routes/routes.js
+++ ChessWiki/routes/routes.js
@@ -11,19 +11,12 @@ const positionOfTheDay = positionsForPosition[1];
 
 router.use(bodyParser.urlencoded({extended: false}));
 
-function positionRender() {
-    router.get("/positions/position:id", function (req, res) {
-        res.render("position", {position: positionsForPosition[req.params.id], positionOfTheDay: positionOfTheDay});
-    });
-}
-
 router.post("/formposition", function (req, res) {
     const positionString = req.body.row8 + "/" + req.body.row7 + "/" + req.body.row6 + "/" + req.body.row5 + "/" + req.body.row4 + "/" + req.body.row3 + "/" + req.body.row2 + "/" + req.body.row1;
     const activeColor = req.body.activeColor;
     const castlingAvailability = req.body.castlingAvailability;
-    const numberOfMoves = parseInt(req.body.numberOfMoves);
+    const numberOfMoves = req.body.numberOfMoves;
     positionsForPosition.push(new dataPositions.Position(positionString, activeColor, castlingAvailability, numberOfMoves));
-    positionRender();
     res.redirect("/positions");
 })
 
@@ -35,14 +28,17 @@ router.get("/positions", function (req, res) {
     res.render("listpositions", {positions: positionsForPosition, positionOfTheDay: positionOfTheDay});
 });
 
-positionRender();
-
-router.get("/openings", function (req, res) {
-    res.render("listopenings", {positionOfTheDay: positionOfTheDay});
+router.get("/positions/position:id", function (req, res) {
+    req.params.id>positionsForPosition.length-1 ? res.render("404") :
+        res.render("position", {position: positionsForPosition[req.params.id], positionOfTheDay: positionOfTheDay});
 });
 
 router.get("/formposition", function (req, res) {
     res.render("formposition", {positionOfTheDay: positionOfTheDay})
+});
+
+router.get("/openings", function (req, res) {
+    res.render("listopenings", {positionOfTheDay: positionOfTheDay});
 });
 
 router.get("/openings/schottische_partie", function (req, res) {
